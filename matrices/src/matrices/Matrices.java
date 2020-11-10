167017ae929cacb8166db5eefc02d80c39ccd1c4
Zacarias18 <pvzacarias18@gmail.com>
2020-11-10 13:47:58 -0400

    creando funcion suma

--- matrices/src/matrices/Matrices.java
+++ matrices/src/matrices/Matrices.java
@@ -1,21 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package matrices;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Zacarias
  */
 public class Matrices {
-
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int M_1[][] = new int [4][4];
+        int M_2[][] = new int [4][4];
+        int M_1M_2 [][] = new int [4][4];
+        int M_3 [][] = new int[][];
+        
+        for (int i = 0; i < M_1.length; i++) {
+            for (int j = 0; j < M_1.length; j++) {
+                M_1[i][j] = (int)(Math.random()*6);
+            }            
+        }
+        for (int i = 0; i < M_2.length; i++) {
+            for (int j = 0; j < M_2.length; j++) {
+                M_2[i][j] = (int)(Math.random()*6);
+            }            
+        }
+        for (int i = 0; i < M_1.length; i++) {
+            for (int j = 0; j < M_2.length; j++) {
+                M_1M_2[i][j] = M_1[i][j] + M_2[i][j];            
+            }
+        }
     }
-    
 }
