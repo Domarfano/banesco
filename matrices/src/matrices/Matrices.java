af2e3c2aed13507b0e6a64ea22a6cf52159f68e3
Zacarias18 <pvzacarias18@gmail.com>
2020-11-10 18:18:12 -0400

    primera funcion lista

--- matrices/src/matrices/Matrices.java
+++ matrices/src/matrices/Matrices.java
@@ -9,15 +9,12 @@ import java.util.Scanner;
  */
 public class Matrices {
     
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner m= new Scanner(System.in);
+        Scanner m = new Scanner(System.in);
         boolean salir = false;
         int opcion;
-        
-        while(!salir){
+
+        while (!salir) {
             System.out.println("1. Operaciones aritmeticas con Matrices");
             System.out.println("2. Rellenar datos de una Matriz");
             System.out.println("3. Validar Matriz identidad");
@@ -25,36 +22,29 @@ public class Matrices {
             System.out.println("5. Escape");
             System.out.println("Usted selecciono:");
             opcion = m.nextInt();
-            
-            switch(opcion){
+
+            switch (opcion) {
                 case 1:
                     System.out.println("OPERACIONES CON MATRICES");
-                    int M_1[][] = new int[2][3];
-                    int M_2[][] = new int[2][3];
-                    int M_1M_2[][] = new int[2][3];
-                    int M_3[][] = new int[3][2];
-                    int M_result[][]= new int[2][2];
-
-                    for (int i = 0; i < M_1.length; i++) {
-                        for (int j = 0; j < M_1.length; j++) {
-                            M_1[i][j] = (int) (Math.random() * 6);
-                        }
-                    }
-                    for (int i = 0; i < M_2.length; i++) {
-                        for (int j = 0; j < M_2.length; j++) {
-                            M_2[i][j] = (int) (Math.random() * 6);
-                        }
-                    }
+                    int[][]M_1 = {{8,2},{9,3}};
+                    int[][]M_2 = {{5,3},{6,8}};
+                    int M_suma[][] = new int[2][2];
+                    int[][] M_3 = {{3,2,3},{2,9,1}};                 
+                    int fM_suma = M_suma.length;
+                    int cM_suma = M_suma[0].length;
+                    int fM_3 = M_3.length;
+                    int cM_3 = M_3[0].length;
+                    
                     for (int i = 0; i < M_1.length; i++) {
                         for (int j = 0; j < M_2.length; j++) {
-                            M_1M_2[i][j] = M_1[i][j] + M_2[i][j];
+                            M_suma[i][j] = M_1[i][j] + M_2[i][j];
                         }
                     }
-                    for (int i = 0; i < M_1.length; i++) {
+                    for(int i = 0; i < M_1.length; i++) {
                         for (int j = 0; j < M_1.length; j++) {
                             System.out.print("[" + M_1[i][j] + "]");
                         }
-                        if (i == 2) {
+                        if (i == 1) {
                             System.out.print("  +  ");
                         } else {
                             System.out.print("     ");
@@ -62,22 +52,31 @@ public class Matrices {
                         for (int j = 0; j < M_2.length; j++) {
                             System.out.print("[" + M_2[i][j] + "]");
                         }
-                        if (i == 2) {
+                        if (i == 1) {
                             System.out.print("  =  ");
                         } else {
                             System.out.print("     ");
                         }
-                        for (int j = 0; j < M_1M_2.length; j++) {
-                            System.out.print("[" + M_1M_2[i][j] + "]");
+                        for (int j = 0; j < M_suma.length; j++) {
+                            System.out.print("[" + M_suma[i][j] + "]");
+                        }
+                        System.out.println("\n");
+                    }
+                    int[][] result = new int[fM_suma][cM_3];
+                    for (int i = 0; i < fM_suma; i++) {
+                        for (int j = 0; j < cM_3; j++) {
+                            for (int k = 0; k < cM_suma; k++) {
+                                result[i][j] += M_suma[i][k] * M_3[k][j];
+                            }
+                        }
+                    }
+                    System.out.println("EL RESULTADO DE (M_1 + M_2) * M_3 ES:");
+                    for (int[] fila : result) {
+                        for (int columna : fila) {
+                            System.out.print("[" + columna + " ]");
                         }
                         System.out.println("");
                     }
-                    for (int i = 0; i < 3; i++) {
-                        for (int j = 0; j < 2; j++) {
-                            M_3[i][j] = (int)(Math.random()*6);
-                        }                        
-                    }
-
                     System.out.println("\n");
                     break;
                 case 2:
